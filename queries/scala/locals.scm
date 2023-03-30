648ae8e385d8137000d826bcf818d795832696d9
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-03-30 18:51:20 +0200

    Working variable scoping

--- queries/scala/locals.scm
+++ queries/scala/locals.scm
@@ -1,30 +1,12 @@
+[(block)
+(template_body)
+(lambda_expression)
+(function_declaration)] @scope
+
+(function_declaration
+  name: (identifier) @definition.function)
+
 (val_definition
   pattern: (identifier) @definition.var)
 
-(infix_expression right: (block)) @test
-
-(class_definition
-  body: (template_body)? @class.inner) @class.outer
-
-(object_definition
-  body: (template_body)? @class.inner) @class.outer
-
-(function_definition
-  body: (indented_block) @function.inner) @function.outer
-
-(function_definition
-  body: (block) @function.inner) @function.outer
-
-(function_definition
-  body: (field_expression) @function.inner) @function.outer
-
-
-(function_definition
-  body: (call_expression) @function.inner) @function.outer
-
-(parameter
-  name: (identifier) @parameter.inner) @parameter.outer
-
-(class_parameter name: (identifier) @parameter.inner) @parameter.outer
-
-(comment) @comment.outer
+(parameters (parameter name: (identifier) @definition.parameter ))
